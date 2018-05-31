ee843dba55e1176e3bf509457f84b730ea83ace5
Mrxpj.github.io <1057961401@qq.com>
2018-05-31 17:58:48 +0800

    index-swiper

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
-fastClick.attach(document.body)
+fastClick.attach(document.body) //解决移动端点击事件300ms延迟
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+		    <!-- slides -->
+		    <swiper-slide v-for="item of swiperList" :key="item.id">
+		    	<img class="swiper-img" :src="item.imgUrl" alt=""/>
+		    </swiper-slide>
+		    <!-- Optional controls -->
+		    <div class="swiper-pagination"  slot="pagination"></div>
+	   </swiper>
+   </div>
+</template>
+
+<script>
+export default {
+	name: 'HomeSwiper',
+	data: function(){
+		return {
+			swiperOption: {
+				pagination: '.swiper-pagination',
+				loop: true
+			},
+			swiperList: [{
+				id: '0001',
+				imgUrl: 'http://img.zcool.cn/community/0128395843a9c9a8012060c80171a9.jpg'
+			}, {
+				id: '0002',
+				imgUrl: 'http://pic.qiantucdn.com/58pic/19/62/85/82458PICSYB_1024.jpg'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+  	background: #fff !important
+  .wrapper
+  	overflow: hidden
+  	width: 100%
+  	height: 0
+  	padding-bottom: 40.5%
+  	background: #eee
+	.swiper-img
+	  width: 100%
+	  height: 136px
+</style>
